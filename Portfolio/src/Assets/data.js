61c5a201236c056c49955e4ab25dc8c35e128a1a
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-03-30 08:57:18 +0530

    skills added

--- Portfolio/src/Assets/data.js
+++ Portfolio/src/Assets/data.js
@@ -9,23 +9,27 @@ export const Projects = [{
   "title": "NEWSEN",
   "description": "A news web app. Its best features are real-time news, 25+ country news and various categories of news are available.",
   "url" : "https://github.com/Durgesh18-p/newser",
-  "imgSrc": newsen
+  "imgSrc": newsen,
+  "skills" : 'React JS, Tailwind CSS, RTK, Firebase'
 },{
   
   "title": "Bought or Not",
   "description": "E-commerce app. User can add products to cart and can remove them from cart. All data is fetched from server.",
   "url" : "https://github.com/Durgesh18-p/E-Commerce-website",
-  "imgSrc": bon
+  "imgSrc": bon,
+  "skills" : 'React JS, CSS, API'
 },{
   
   "title": "CRUD App",
   "description": "Task management app using React JS, Redux toolkit. Create, Update, Read and Delete operations are allowed.",
   "url" : "https://github.com/Durgesh18-p/React_JS_CRUD_App",
-  "imgSrc": crud
+  "imgSrc": crud,
+  "skills" : 'React JS, Tailwind CSS, RTK'
 },{
   
   "title": "Advice App",
   "description": "Your guide in difficult situation. All advices are fetched from server. User can save the advices.",
   "url" : "https://github.com/Durgesh18-p/Advice-App",
-  "imgSrc": advice
+  "imgSrc": advice,
+  "skills" : 'React JS, CSS, API'
 },]
