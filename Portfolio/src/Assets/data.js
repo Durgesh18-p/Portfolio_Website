253ab4876ed9540b6aad373e953329a1b19f7279
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-03-30 08:47:55 +0530

    project info added

---
+++ Portfolio/src/Assets/data.js
@@ -0,0 +1,31 @@
+import newsen from "./newsen.mp4"
+import bon from "./bought_or_bot.mp4"
+import crud from "./crud_app.mp4"
+import advice from "./advice_app.mp4"
+
+
+export const Projects = [{
+  
+  "title": "NEWSEN",
+  "description": "A news web app. Its best features are real-time news, 25+ country news and various categories of news are available.",
+  "url" : "https://github.com/Durgesh18-p/newser",
+  "imgSrc": newsen
+},{
+  
+  "title": "Bought or Not",
+  "description": "E-commerce app. User can add products to cart and can remove them from cart. All data is fetched from server.",
+  "url" : "https://github.com/Durgesh18-p/E-Commerce-website",
+  "imgSrc": bon
+},{
+  
+  "title": "CRUD App",
+  "description": "Task management app using React JS, Redux toolkit. Create, Update, Read and Delete operations are allowed.",
+  "url" : "https://github.com/Durgesh18-p/React_JS_CRUD_App",
+  "imgSrc": crud
+},{
+  
+  "title": "Advice App",
+  "description": "Your guide in difficult situation. All advices are fetched from server. User can save the advices.",
+  "url" : "https://github.com/Durgesh18-p/Advice-App",
+  "imgSrc": advice
+},]
